58d7dcd77ad53e9b031c51b509b4024fa97f9b03
Bubula, Michal <michal.bubula@sbb.spk-berlin.de>
2025-02-25 16:48:21 +0100

    cleaning up

--- src/sbb_ocr/ocr_wc_analysis.py
+++ src/sbb_ocr/ocr_wc_analysis.py
@@ -56,35 +56,6 @@ def plot_density(ax, data, title, xlabel, ylabel, density_color, legend_loc):
     ax.plot(x_range, density_values, color=density_color, lw=2)
     ax.legend(loc=legend_loc)
 
-def plot_histogram_with_density(ax, data, bins, title, xlabel, ylabel, color, density_color, legend_loc):
-    ax.hist(data, bins=bins, color=color, edgecolor="black", alpha=0.6, density=True)
-    ax.set_title(title)
-    ax.set_xlabel(xlabel)
-    ax.set_ylabel(ylabel)
-    ax.set_xlim(0, 1.0)
-    ax.set_xticks(np.arange(0, 1.1, 0.1))
-    ax.grid(axis="y", alpha=0.75)
-    
-    kde = gaussian_kde(data)
-    x_range = np.linspace(0, 1, 100)
-    density_values = kde(x_range)
-    max_kde_density = np.max(density_values) 
-    ax.plot(x_range, kde(x_range), color=density_color, lw=2, label="Density Plot")
-    round_up_half_integer = (int(max_kde_density * 2) + (max_kde_density * 2 % 1 > 0)) / 2
-    ax.set_ylim(0, round_up_half_integer)
-    ax.set_yticks(np.arange(0, round_up_half_integer+0.01, 0.5))
-    ax.legend(loc=legend_loc)
-
-def plot_boxplot(ax, data, title, ylabel, box_colors):
-    bp = ax.boxplot(data, patch_artist=True, medianprops=dict(color="black", linestyle="--",))
-    for patch, color in zip(bp["boxes"], box_colors):
-        patch.set_facecolor(color)   
-    ax.set_title(title)
-    ax.set_ylabel(ylabel)
-    ax.set_xticks([1, 2], ["Mean", "Standard Deviation"])
-    ax.set_yticks(np.arange(0, 1.1, 0.1))
-    ax.grid(axis="y", alpha=0.75)
-
 @contextmanager
 def load_csv(csv_file):
     with open(csv_file, 'r') as f:
@@ -116,16 +87,14 @@ def plot_everything(csv_files : list[str], plot_file="statistics_results.jpg"):
                         ppn_page = f'{row[0]}_{row[1]}_{row[2]}'
                         ppn = f'{row[0]}'
                         all_results.append([ppn, ppn_page, mean_word, median_word, standard_deviation_word, mean_textline, median_textline, standard_deviation_textline])
-                        
-                    
-                        
+                                               
             except csv.Error as e:
                 print(f"CSV error: {e} in file: {csv_file}. \nIncrease the CSV field size limit!")
                 break
             progbar.update(1)
     
     results_df = pd.DataFrame(all_results, columns=["ppn", "ppn_page", "mean_word", "median_word", "standard_deviation_word", "mean_textline", "median_textline", "standard_deviation_textline"])
-        
+
     
     print("Statistics results:\n", results_df)
 
@@ -190,36 +159,3 @@ def plot_everything(csv_files : list[str], plot_file="statistics_results.jpg"):
     plt.tight_layout(pad=1.0)
     plt.savefig(plot_file)
     plt.show()
-    
-    
-    """plot_histogram_with_density(axs[0, 0], results_df["mean_word"], np.arange(0, 1.1, 0.05), 
-                                "Mean Word Confidence Scores", 
-                                "Mean Word Confidence", "Frequency", 
-                                plot_colors["word"]["mean"], plot_colors["word"]["mean_density"], legend_loc="upper left")
-
-    plot_boxplot(axs[0, 1], [results_df["mean_word"], results_df["standard_deviation_word"]], 
-                 "Mean and Standard Deviation of Word Confidence Scores", 
-                 "Confidence Scores", [plot_colors["word"]["mean"], plot_colors["word"]["std"]])
-
-    plot_histogram_with_density(axs[0, 2], results_df["standard_deviation_word"], np.arange(0, 1.1, 0.05), 
-                                "Standard Deviation of Word Confidence Scores", 
-                                "Standard Deviation", "Frequency", 
-                                plot_colors["word"]["std"], plot_colors["word"]["std_density"], legend_loc="upper right")
-
-    plot_histogram_with_density(axs[1, 0], results_df["mean_textline"], np.arange(0, 1.1, 0.05), 
-                                "Mean Textline Confidence Scores", 
-                                "Mean Textline Confidence", "Frequency", 
-                                plot_colors["textline"]["mean"], plot_colors["textline"]["mean_density"], legend_loc="upper left")
-
-    plot_boxplot(axs[1, 1], [results_df["mean_textline"], results_df["standard_deviation_textline"]], 
-                 "Mean and Standard Deviation of Textline Confidence Scores", 
-                 "Confidence Scores", [plot_colors["textline"]["mean"], plot_colors["textline"]["std"]])
-
-    plot_histogram_with_density(axs[1, 2], results_df["standard_deviation_textline"], np.arange(0, 1.1, 0.05), 
-                                "Standard Deviation of Textline Confidence Scores", 
-                                "Standard Deviation", "Frequency", 
-                                plot_colors["textline"]["std"], plot_colors["textline"]["std_density"], legend_loc="upper right")
-    
-    plt.tight_layout(pad=1.5)
-    plt.savefig(plot_file)
-    plt.show()"""
